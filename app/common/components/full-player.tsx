7dc40c6e1f3e88433d195213ca2d95f88b74a165
Yungi Jung <hubiwibe@gmail.com>
2025-05-22 21:18:13 +0900

    style: full-player 최대 크기 조절

--- app/common/components/full-player.tsx
+++ app/common/components/full-player.tsx
@@ -1,19 +1,14 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '~/common/components/ui/drawer';
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from '~/common/components/ui/drawer';
 
 export default function FullPlayer() {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(true);
 
   return (
-    <Drawer defaultOpen={true} onClose={() => setTimeout(() => navigate(-1), 100)}>
-      <DrawerContent className="h-screen w-full">
+    <Drawer open={open} onOpenChange={setOpen} onClose={() => setTimeout(() => navigate(-1), 100)}>
+      <DrawerContent className="mx-auto h-screen max-w-xl">
         <DrawerHeader>
           <DrawerTitle>track name</DrawerTitle>
           <DrawerDescription>track description</DrawerDescription>
