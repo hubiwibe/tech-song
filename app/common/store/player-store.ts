1e6226b40aeaf31fc312f963b1a1e6386082cd0f
Yungi Jung <hubiwibe@gmail.com>
2025-05-23 12:58:20 +0900

    chore: mockTracks 아이디 변경

--- app/common/store/player-store.ts
+++ app/common/store/player-store.ts
@@ -4,7 +4,7 @@ import type { Track } from '~/common/types/track';
 
 const mockTracks: Track[] = [
   {
-    id: 0,
+    id: 1,
     title: '리액트 리스트에 왜 key를 사용해야 하나요?',
     content:
       'React에서 key는 리스트의 각 아이템을 고유하게 식별하여, UI 변경 시 효율적이고 정확한 업데이트를 가능하게 하는 중요한 역할을 합니다.\nkey 없이는 React가 어떤 항목이 변경되었는지 판단하기 어려워 성능 저하와 버그가 생길 수 있습니다.',
@@ -13,7 +13,7 @@ const mockTracks: Track[] = [
     updatedAt: '2025-05-22',
   },
   {
-    id: 1,
+    id: 2,
     title: '리액트에서 합성 이벤트란 무엇인가?',
     content:
       'React 합성 이벤트는 브라우저별 이벤트 차이를 감추고, 효율적인 이벤트 관리와 일관된 인터페이스를 제공하는 React만의 이벤트 래퍼 객체입니다.\n개발자는 합성 이벤트를 통해 편리하고 안정적으로 이벤트 처리를 할 수 있습니다.',
@@ -22,7 +22,7 @@ const mockTracks: Track[] = [
     updatedAt: '2025-05-22',
   },
   {
-    id: 2,
+    id: 3,
     title: 'forwardRef란 무엇인가?',
     content:
       'forwardRef는 부모 컴포넌트가 자식 함수형 컴포넌트 내부의 DOM 요소에 직접 접근할 수 있도록 ref를 전달하는 React 기능입니다.\n이를 통해 함수형 컴포넌트에서도 ref를 사용할 수 있게 해 줍니다.',
