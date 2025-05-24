7e723d504dd68002d32966702f7bfa1ed16ce982
Yungi Jung <hubiwibe@gmail.com>
2025-05-24 18:23:21 +0900

    feat: full-player 페이지에 player-controller 추가

--- app/common/components/full-player.tsx
+++ app/common/components/full-player.tsx
@@ -1,16 +1,36 @@
-import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from '~/common/components/ui/drawer';
+import { useNavigate } from 'react-router';
+import PlayerController from '~/common/components/player-controller';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from '~/common/components/ui/drawer';
 import { usePlayerStore } from '~/common/store/player-store';
 
 export default function FullPlayer() {
-  const { isFullPlayerOpen, currentTrack } = usePlayerStore();
+  const navigate = useNavigate();
+  const { isFullPlayerOpen, currentTrack, setIsFullPlayerOpen } = usePlayerStore();
+
+  const handleClose = () => {
+    setIsFullPlayerOpen(false);
+    setTimeout(() => {
+      navigate(-1);
+    }, 150);
+  };
 
   return (
-    <Drawer open={isFullPlayerOpen}>
+    <Drawer open={isFullPlayerOpen} onOpenChange={handleClose}>
       <DrawerContent className="mx-auto h-full max-w-xl">
         <DrawerHeader>
           <DrawerTitle>{currentTrack?.title}</DrawerTitle>
           <DrawerDescription className="whitespace-pre-wrap">{currentTrack?.content}</DrawerDescription>
         </DrawerHeader>
+        <DrawerFooter className="bg-gray-100 dark:bg-gray-900">
+          <PlayerController />
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
