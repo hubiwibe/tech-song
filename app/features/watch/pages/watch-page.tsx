06de2afb60c3a7c41119ed472fb3bab8895319a6
Yungi Jung <hubiwibe@gmail.com>
2025-05-24 18:20:24 +0900

    feat: watch 페이지 렌더링 시 full-player open

--- app/features/watch/pages/watch-page.tsx
+++ app/features/watch/pages/watch-page.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from 'react';
 import FullPlayer from '~/common/components/full-player';
+import { usePlayerStore } from '~/common/store/player-store';
 
 export default function WatchPage() {
+  const { setIsFullPlayerOpen } = usePlayerStore();
+
+  useEffect(() => {
+    setIsFullPlayerOpen(true);
+
+    return () => {
+      setIsFullPlayerOpen(false);
+    };
+  }, []);
+
   return <FullPlayer />;
 }
