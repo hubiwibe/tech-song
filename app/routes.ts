a1744b1e93eb5865c7c0ba69e5601c2d03cf0ce6
Yungi Jung <hubiwibe@gmail.com>
2025-05-25 16:34:57 +0900

    chore: playlist 페이지 param명 변경

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, prefix, route } from '@react-router/dev/routes
 export default [
   index('common/pages/home-page.tsx'),
   ...prefix('watch', [route('/:trackId', 'features/watch/pages/watch-page.tsx')]),
-  ...prefix('playlist', [route('/:listId', 'features/playlist/pages/playlist-page.tsx')]),
+  ...prefix('playlist', [route('/:playlistId', 'features/playlist/pages/playlist-page.tsx')]),
 ] satisfies RouteConfig;
