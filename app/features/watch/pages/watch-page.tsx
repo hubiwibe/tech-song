f2e5eaf0277f45fa2df5fa578697d3af6e38faf0
Yungi Jung <hubiwibe@gmail.com>
2025-05-25 16:42:30 +0900

    feat: watch 페이지 로드시 playlist 및 track mock 데이터 적용

--- app/features/watch/pages/watch-page.tsx
+++ app/features/watch/pages/watch-page.tsx
@@ -1,9 +1,38 @@
-import { useEffect } from 'react';
 import FullPlayer from '~/common/components/full-player';
 import { usePlayerStore } from '~/common/store/player-store';
+import type { Route } from './+types/watch-page';
+import { getPlaylistByTrackId } from '~/api/playlist';
+import { useEffect } from 'react';
 
-export default function WatchPage() {
-  const { setIsFullPlayerOpen } = usePlayerStore();
+export const meta: Route.MetaFunction = ({ data }) => {
+  return [{ title: `Playlist - ${data.playlist?.title ?? 'Not Found'}` }];
+};
+
+export const loader = async ({ params }: Route.LoaderArgs) => {
+  if (!params.trackId) {
+    return { playlist: null, track: null };
+  }
+  const playlist = await getPlaylistByTrackId(params.trackId);
+  if (!playlist) {
+    return { playlist: null, track: null };
+  }
+  const track = playlist.playlistTracks.find(track => track.id === params.trackId);
+  if (!track) {
+    return { playlist: null, track: null };
+  }
+  return { playlist, track };
+};
+
+export default function WatchPage({ loaderData }: Route.ComponentProps) {
+  const { setCurrentTrack, setPlaylist, setIsFullPlayerOpen } = usePlayerStore();
+  const { playlist, track } = loaderData;
+
+  useEffect(() => {
+    if (track && playlist) {
+      setCurrentTrack(track);
+      setPlaylist(playlist);
+    }
+  }, []);
 
   useEffect(() => {
     setIsFullPlayerOpen(true);
