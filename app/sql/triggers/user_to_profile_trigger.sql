92fc4e92d3cfdfa8c0bde6910deed3a36da551c0
Yungi Jung <hubiwibe@gmail.com>
2025-05-31 21:51:21 +0900

    feat: user to profile trigger 추가

---
+++ app/sql/triggers/user_to_profile_trigger.sql
@@ -0,0 +1,19 @@
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = ''
+as $$
+begin
+    if new.raw_app_meta_data is not null then
+        if new.raw_app_meta_data ? 'provider' and new.raw_app_meta_data->>'provider' = 'email' then
+            insert into public.profiles (profile_id, name) values (new.id, 'Anonymous');
+        end if;
+    end if;
+    return new;
+end;
+$$;
+
+create trigger user_to_profile_trigger
+after insert on auth.users
+for each row execute function handle_new_user();
