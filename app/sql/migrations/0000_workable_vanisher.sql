3792d847520eeb00ab5a5a04446aa978e648b2f4
Yungi Jung <hubiwibe@gmail.com>
2025-05-31 22:29:52 +0900

    refactor: migration 초기화

---
+++ app/sql/migrations/0000_workable_vanisher.sql
@@ -0,0 +1,98 @@
+CREATE TABLE "playback_histories" (
+	"playlist_id" bigint,
+	"profile_id" uuid,
+	"track_id" bigint,
+	"position" integer DEFAULT 0 NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "playlist_tracks" (
+	"playlist_id" bigint,
+	"track_id" bigint,
+	"order" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "playlist_views" (
+	"playlist_id" bigint,
+	"profile_id" uuid,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "playlists" (
+	"playlist_id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "playlists_playlist_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
+	"title" text NOT NULL,
+	"description" text,
+	"is_public" boolean DEFAULT false NOT NULL,
+	"stats" jsonb DEFAULT '{"views":0,"likes":0}'::jsonb NOT NULL,
+	"profile_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "categories" (
+	"category_id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "categories_category_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
+	"title" text NOT NULL,
+	"description" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "tags" (
+	"tag_id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "tags_tag_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
+	"name" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "tags_name_unique" UNIQUE("name")
+);
+--> statement-breakpoint
+CREATE TABLE "track_categories" (
+	"track_id" bigint,
+	"category_id" bigint,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "track_categories_track_id_category_id_pk" PRIMARY KEY("track_id","category_id")
+);
+--> statement-breakpoint
+CREATE TABLE "track_tags" (
+	"track_id" bigint,
+	"tag_id" bigint,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "track_tags_track_id_tag_id_pk" PRIMARY KEY("track_id","tag_id")
+);
+--> statement-breakpoint
+CREATE TABLE "tracks" (
+	"track_id" bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "tracks_track_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START WITH 1 CACHE 1),
+	"title" text NOT NULL,
+	"content" text NOT NULL,
+	"stats" jsonb DEFAULT '{"views":0,"likes":0}'::jsonb NOT NULL,
+	"profile_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "profiles" (
+	"profile_id" uuid PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "playback_histories" ADD CONSTRAINT "playback_histories_playlist_id_playlists_playlist_id_fk" FOREIGN KEY ("playlist_id") REFERENCES "public"."playlists"("playlist_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playback_histories" ADD CONSTRAINT "playback_histories_profile_id_profiles_profile_id_fk" FOREIGN KEY ("profile_id") REFERENCES "public"."profiles"("profile_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playback_histories" ADD CONSTRAINT "playback_histories_track_id_tracks_track_id_fk" FOREIGN KEY ("track_id") REFERENCES "public"."tracks"("track_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playlist_tracks" ADD CONSTRAINT "playlist_tracks_playlist_id_playlists_playlist_id_fk" FOREIGN KEY ("playlist_id") REFERENCES "public"."playlists"("playlist_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playlist_tracks" ADD CONSTRAINT "playlist_tracks_track_id_tracks_track_id_fk" FOREIGN KEY ("track_id") REFERENCES "public"."tracks"("track_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playlist_views" ADD CONSTRAINT "playlist_views_playlist_id_playlists_playlist_id_fk" FOREIGN KEY ("playlist_id") REFERENCES "public"."playlists"("playlist_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playlist_views" ADD CONSTRAINT "playlist_views_profile_id_profiles_profile_id_fk" FOREIGN KEY ("profile_id") REFERENCES "public"."profiles"("profile_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "playlists" ADD CONSTRAINT "playlists_profile_id_profiles_profile_id_fk" FOREIGN KEY ("profile_id") REFERENCES "public"."profiles"("profile_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "track_categories" ADD CONSTRAINT "track_categories_track_id_tracks_track_id_fk" FOREIGN KEY ("track_id") REFERENCES "public"."tracks"("track_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "track_categories" ADD CONSTRAINT "track_categories_category_id_categories_category_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("category_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "track_tags" ADD CONSTRAINT "track_tags_track_id_tracks_track_id_fk" FOREIGN KEY ("track_id") REFERENCES "public"."tracks"("track_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "track_tags" ADD CONSTRAINT "track_tags_tag_id_tags_tag_id_fk" FOREIGN KEY ("tag_id") REFERENCES "public"."tags"("tag_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "tracks" ADD CONSTRAINT "tracks_profile_id_profiles_profile_id_fk" FOREIGN KEY ("profile_id") REFERENCES "public"."profiles"("profile_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_profile_id_users_id_fk" FOREIGN KEY ("profile_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;
