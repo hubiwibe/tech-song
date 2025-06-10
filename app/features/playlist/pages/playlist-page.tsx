319ab2332acec40d9e885a718f5b65ed36155b8b
Yungi Jung <hubiwibe@gmail.com>
2025-06-10 11:58:18 +0900

    fix: playlist 페이지 로드시 playilst 정보 추가

--- app/features/playlist/pages/playlist-page.tsx
+++ app/features/playlist/pages/playlist-page.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import Playlist from '~/features/playlist/components/playlist';
 import { ScrollArea } from '~/common/components/ui/scroll-area';
 import { Button } from '~/common/components/ui/button';
-import { getPlaylistTracksByPlaylistId } from '~/features/playlist/queries';
+import { getPlaylistById, getPlaylistTracksByPlaylistId } from '~/features/playlist/queries';
 
 export const meta: Route.MetaFunction = ({ data }) => {
   return [{ title: `Playlist - ${data.playlistTracks?.[0]?.title ?? 'Not Found'}` }];
@@ -14,15 +14,17 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
   if (!params.playlistId) {
     return { playlist: null };
   }
-  const playlistTracks = await getPlaylistTracksByPlaylistId(Number(params.playlistId));
-  return { playlistTracks };
+  const playlistId = Number(params.playlistId);
+  const playlist = await getPlaylistById(playlistId);
+  const playlistTracks = await getPlaylistTracksByPlaylistId(playlistId);
+  return { playlist, playlistTracks };
 };
 
 export default function PlaylistPage({ loaderData }: Route.ComponentProps) {
   const navigate = useNavigate();
-  const { playlistTracks } = loaderData;
+  const { playlist, playlistTracks } = loaderData;
 
-  if (!playlistTracks) {
+  if (!playlist) {
     return <div>Playlist not found</div>;
   }
 
@@ -30,21 +32,17 @@ export default function PlaylistPage({ loaderData }: Route.ComponentProps) {
     navigate(`/watch/${playlistTracks[0].trackId}`);
   };
 
-  if (playlistTracks.length === 0) {
-    return <div>Playlist not found</div>;
-  }
-
   return (
     <section className="px-3 py-6 h-full">
       <header className="mb-8 flex items-center gap-4">
         <ChevronLeft className="w-10 h-10 cursor-pointer" onClick={() => navigate(-1)} />
-        <h1 className="text-4xl font-bold">{playlistTracks[0].title}</h1>
+        <h1 className="text-4xl font-bold">{playlist.title}</h1>
       </header>
       <ScrollArea className="flex flex-col gap-4 h-96 rounded-md p-2 border">
         <Playlist playlistTracks={playlistTracks} />
       </ScrollArea>
       <div className="fixed left-0 right-0 bottom-12 flex items-center justify-center p-4 max-w-xl mx-auto">
-        <Button className="w-full cursor-pointer" onClick={handleListen}>
+        <Button className="w-full cursor-pointer" onClick={handleListen} disabled={playlistTracks.length === 0}>
           <Play className="size-5" />
           <span>Listen</span>
         </Button>
