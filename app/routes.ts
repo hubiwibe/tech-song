90d877e481d7d89eb4975c7b2fed4d491f6a3835
Yungi Jung <hubiwibe@gmail.com>
2025-06-10 14:19:36 +0900

    feat: cron job 페이지 라우트 설정

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
   index('common/pages/home-page.tsx'),
   ...prefix('watch', [route('/:trackId', 'features/watch/pages/watch-page.tsx')]),
   ...prefix('playlist', [route('/:playlistId', 'features/playlist/pages/playlist-page.tsx')]),
+  ...prefix('cron-job', [route('/', 'common/pages/cron-job-page.tsx')]),
 ] satisfies RouteConfig;
