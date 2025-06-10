ce78fe564b396033ff1393340d0faa64d70af768
Yungi Jung <hubiwibe@gmail.com>
2025-06-10 11:56:33 +0900

    feat: playlist id로 조회하는 함수 추가

--- app/features/playlist/queries.ts
+++ app/features/playlist/queries.ts
@@ -24,6 +24,19 @@ export const getPlaylistByTrackId = async (trackId: number): Promise<Playlist | 
     .then(result => result[0] ?? null);
 };
 
+export const getPlaylistById = async (playlistId: number): Promise<Playlist | null> => {
+  return await db
+    .select({
+      playlistId: playlists.playlist_id,
+      title: playlists.title,
+      description: playlists.description,
+    })
+    .from(playlists)
+    .where(eq(playlists.playlist_id, playlistId))
+    .limit(1)
+    .then(result => result[0] ?? null);
+};
+
 export const getPlaylistTracksByPlaylistId = async (playlistId: number): Promise<PlaylistTrack[]> => {
   return await db
     .select({
