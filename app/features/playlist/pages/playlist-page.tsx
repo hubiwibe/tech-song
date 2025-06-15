30af255d84df539e357b29c431d8fd07f3c95be9
Yungi Jung <hubiwibe@gmail.com>
2025-06-15 22:05:54 +0900

    style: playlist 페이지에 magic-card 적용

--- app/features/playlist/pages/playlist-page.tsx
+++ app/features/playlist/pages/playlist-page.tsx
@@ -5,6 +5,9 @@ import Playlist from '~/features/playlist/components/playlist';
 import { ScrollArea } from '~/common/components/ui/scroll-area';
 import { Button } from '~/common/components/ui/button';
 import { getPlaylistById, getPlaylistTracksByPlaylistId } from '~/features/playlist/queries';
+import { MagicCard } from '~/common/components/magicui/magic-card';
+import { Card } from '~/common/components/ui/card';
+import { getRandomColors } from '~/lib/utils';
 
 export const meta: Route.MetaFunction = ({ data }) => {
   return [{ title: `Playlist - ${data.playlistTracks?.[0]?.title ?? 'Not Found'}` }];
@@ -38,9 +41,13 @@ export default function PlaylistPage({ loaderData }: Route.ComponentProps) {
         <ChevronLeft className="w-10 h-10 cursor-pointer" onClick={() => navigate(-1)} />
         <h1 className="text-4xl font-bold">{playlist.title}</h1>
       </header>
-      <ScrollArea className="flex flex-col gap-4 h-96 rounded-md p-2 border">
-        <Playlist playlistTracks={playlistTracks} />
-      </ScrollArea>
+      <Card className="p-0 shadow-none border-none">
+        <MagicCard className="p-0" gradientColor="#D9D9D955">
+          <ScrollArea className="flex flex-col gap-4 h-96 rounded-md p-2 border-none">
+            <Playlist playlistTracks={playlistTracks} />
+          </ScrollArea>
+        </MagicCard>
+      </Card>
       <div className="fixed left-0 right-0 bottom-12 flex items-center justify-center p-4 max-w-xl mx-auto">
         <Button className="w-full cursor-pointer" onClick={handleListen} disabled={playlistTracks.length === 0}>
           <Play className="size-5" />
