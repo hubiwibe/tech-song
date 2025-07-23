9b36f35e7f093293ade1491d58d44026b35c8324
ygjung <hubiwibe@gmail.com>
2025-07-23 09:01:57 +0900

    feat: add routes for login and signup pages

--- app/routes.ts
+++ app/routes.ts
@@ -6,4 +6,6 @@ export default [
   ...prefix('playlist', [route('/:playlistId', 'features/playlist/pages/playlist-page.tsx')]),
   ...prefix('cron-job', [route('/', 'common/pages/cron-job-page.tsx')]),
   route('/welcome', 'features/users/pages/welcome-page.tsx'),
+  route('/login', 'features/auth/pages/login-page.tsx'),
+  route('/signup', 'features/auth/pages/signup-page.tsx'),
 ] satisfies RouteConfig;
